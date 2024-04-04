34d0b16ea1023161efc69ab26bdafb90e725c87e
Achim Grolimund <ac.grolimund@gmail.com>
2024-04-04 13:08:12 +0200

    add wiki to allowed CORS

--- api/middleware/cors_validator.go
+++ api/middleware/cors_validator.go
@@ -1,18 +1,27 @@
 package middleware
 
 import (
+	"github.com/achimgrolimund/csp-report-api/pkg/logger"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"net/http"
 )
 
 func CORSValidator() gin.HandlerFunc {
+	log, err := logger.NewLogger()
+	if err != nil {
+		panic(err)
+	}
 	return func(c *gin.Context) {
 		origin := c.Request.Header.Get("Origin")
 		if origin != "" {
-			if origin != "http://localhost" && origin != "https://groltech.ch" && origin != "https://grolimund-solutions.ch" && origin != "https://csp.groltech.ch" {
+			if origin != "http://localhost" && origin != "https://groltech.ch" && origin != "https://grolimund-solutions.ch" && origin != "https://wiki.groltech.ch" {
 				c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"message": "Not allowed CORS origin."})
+				log.Error("Not allowed CORS origin", zap.String("origin", origin))
 				return
 			}
+		} else {
+			log.Error("No CORS origin header found")
 		}
 		c.Next()
 	}
