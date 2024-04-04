564952d186bf998718120d1dba5161dc2ca6156d
Achim Grolimund <ac.grolimund@gmail.com>
2024-04-04 10:44:57 +0200

    add CORS

--- api/middleware/cors_validator.go
+++ api/middleware/cors_validator.go
@@ -9,7 +9,7 @@ func CORSValidator() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		origin := c.Request.Header.Get("Origin")
 		if origin != "" {
-			if origin != "http://localhost" && origin != "http://grolimund-solutions.ch" && origin != "https://grolimund-solutions.ch" {
+			if origin != "http://localhost" && origin != "http://grolimund-solutions.ch" && origin != "https://grolimund-solutions.ch" && origin != "https://csp.groltech.ch" {
 				c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"message": "Not allowed CORS origin."})
 				return
 			}
