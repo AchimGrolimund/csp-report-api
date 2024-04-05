4531a5354c44bf242c3ecfd0eaad2eacde18f85e
Achim Grolimund <ac.grolimund@gmail.com>
2024-04-05 21:28:53 +0200

    add new CORS

--- api/middleware/cors_validator.go
+++ api/middleware/cors_validator.go
@@ -15,7 +15,7 @@ func CORSValidator() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		origin := c.Request.Header.Get("Origin")
 		if origin != "" {
-			if origin != "http://localhost" && origin != "https://groltech.ch" && origin != "https://grolimund-solutions.ch" && origin != "https://wiki.groltech.ch" && origin != "https://hoas.groltech.ch" {
+			if origin != "http://localhost" && origin != "https://groltech.ch" && origin != "https://grolimund-solutions.ch" && origin != "https://wiki.groltech.ch" && origin != "https://hoas.groltech.ch" && origin != "https://ansible.groltech.ch" {
 				c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"message": "Not allowed CORS origin."})
 				log.Error("Not allowed CORS origin", zap.String("origin", origin))
 				return
