767248a6b8d9feede1f0770b57863b362beac9e3
Achim Grolimund <ac.grolimund@gmail.com>
2024-05-13 16:12:21 +0200

    add unix time to payload

--- pkg/api/handler.go
+++ pkg/api/handler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/achimgrolimund/csp-report-api/pkg/domain/csp_report"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 type Handler struct {
@@ -31,6 +32,9 @@ func (h *Handler) SaveReportHandler(c *gin.Context) {
 	// Fügen Sie die Client-IP und den User-Agent zum Payload hinzu
 	report.Report.ClientIP = clientIP
 	report.Report.UserAgent = userAgent
+
+	// Fügen Sie die Unix-Time zum Payload hinzu
+	report.Report.ReportTime = int(time.Now().Unix())
 
 	if err := h.service.SaveReport(c.Request.Context(), &report); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
